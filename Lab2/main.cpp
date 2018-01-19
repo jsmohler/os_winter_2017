658ed798dd5dfabda9522790bf8a851c4d3adc6c
Jordan Mohler <Jordan.Mohler@du.edu>
2018-01-19 16:10:52 -0700

    Lab2

---
+++ Lab2/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Jordan Mohler, Cedric Smith, Lexie Hermosura
+ *
+ * Created on January 19, 2018, 4:09 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
