2fb7365773b908af724b543fddc64b4102600061
Cedric Smith <cedric.m.smith@gmail.com>
2018-02-28 22:49:37 -0700

    Completed Lab5

--- Lab5/Lab5.cpp
+++ Lab5/Lab5.cpp
@@ -1,15 +1,17 @@
-// Lab 5 - COMP3361 Operating Systems Winter 2018
-//
-// Implementation of deadlock detection algorithm.
-// Code is provided to read a system configuration
-// from the input file (specified by the program
-// parameter). You should fill in code as noted by
-// the "TODO" blocks to detect deadlocks. See the
-// assignment for details on the output format.
-//
-// Template author: Mike Goss (mikegoss@cs.du.edu)
-//
-// Student name: [your name here]
+/* 
+ * Lab 5 - COMP3361 Operating Systems Winter 2018
+ * 
+ * Implementation of deadlock detection algorithm.
+ * Code is provided to read a system configuration
+ * from the input file (specified by the program
+ * parameter). You should fill in code as noted by
+ * the "TODO" blocks to detect deadlocks. See the
+ * assignment for details on the output format.
+ * 
+ * Template author: Mike Goss (mikegoss@cs.du.edu)
+ * 
+ * Student name: Jordan Mohler, Cedric Smith, Lexie Hermosura
+*/
 
 #include <fstream>
 #include <iostream>
@@ -24,116 +26,77 @@ using std::setw;
 namespace {
 
 // Define maximum supported data sizes.
-const int kMaxProcesses = 50;  // maximum number of processes
-const int kMaxResources = 50;  // maximum number of resources
+const int kMaxProcesses = 50;  // Maximum number of processes
+const int kMaxResources = 50;  // Maximum number of resources
 
 // Define arrays for algorithm
 int Available[kMaxResources];
 int Allocation[kMaxProcesses][kMaxResources];
 int Request[kMaxProcesses][kMaxResources];
 
-int numProcesses;   // actual number of processes
-int numResources;   // actual number of resources
+int numProcesses;   // Actual number of processes
+int numResources;   // Actual number of resources
 
 
-// IsRequestLessEqual - compare row of Request array to Available
 //
+// IsRequestLessEqual - compare row of Request array to Available
+// 
 // @param i - index of row in Request array
-// @returns true if every element of Request[i] is <= corresponding element
-//          of Available, false otherwise.
+// @return true if every element of Request[i] is <= corresponding element
+//         of Available, false otherwise.
+//
 bool IsRequestLessEqual(int i) {
   bool result = true;
-  //go through all of the resources since the rows' cells consist of # of requests to each resource
-  for(int j=0; j<numResources; j++)
-  {
-//      cout << "Request[" << j << "]: " << Request[i][j] <<"\n";
-//      cout << "Available[" << j << "]: " << Available[j] << "\n";
-      if(Request[i][j]>Available[j])
-      {
-          result = false;
-         
-      }
-      
+  // Go through all of the resources since the rows' cells consist of # of requests to each resource
+  for(int j=0; j<numResources; j++){
+      if(Request[i][j]>Available[j]){
+          result = false;  
+      } 
   }
-  
-//  //test to see if correct
-//  cout << "Is Request <= Available? ";
-//  if(result)
-//      cout << "True.\n";
-//  else
-//      cout << "False.\n";
-  
   return result;
 }
 
-// AddToAvailable - add each element in Allocation[i] to corresponding
-//   element in Available
-//
+// AddToAvailable - add each element in Allocation[i] to corresponding element in Available
+// 
 // @param i - index of row in Allocation array
+// 
 void AddToAvailable(int i) {
-    for(int j=0; j<numResources; j++)
-    {
+    for(int j=0; j<numResources; j++){
         Available[j] = Available[j] + Allocation[i][j];
 
-        
-//         //test1
-//        cout << Available[j] << " + " << Allocation[i][j] << " = ";
-//        Available[j] = Available[j] + Allocation[i][j];
-//        //test2
-//        cout << Available[j] << "\n";
     }    
 }
 
 // PrintDeadlocks - print indices of deadlocked processes
-//
-void PrintDeadlocks(void) 
-{
+void PrintDeadlocks(void) {
     bool Marked[numProcesses];
-    //make all processes unmarked
-    for(int j = 0; j<numProcesses; j++)
-    {
+    // Make all processes unmarked
+    for(int j = 0; j<numProcesses; j++){
         Marked[j] = false;
     }
-    
-    //MAYBE need to revise this later; not sure if it will iterate the correct number of times
-    
-    for(int j=0; j<numProcesses; j++)
-    {
-        //if the process is not marked and Request is less than or equal to Available
-        if(!(Marked[j])&&(IsRequestLessEqual(j)))
-        {
-            //add to available resources and mark process
+    for(int j=0; j<numProcesses; j++){
+        // If the process is not marked and Request is less than or equal to Available
+        if(!(Marked[j])&&(IsRequestLessEqual(j))){
+            // Add to available resources and mark process
             AddToAvailable(j);
-            Marked[j] = true;
-            
-            
+            Marked[j] = true; 
         }
-        
     }
     
-    //go through Marked array and see which are unmarked, which are deadlocks
- 
-    for(int j=0; j<numProcesses; j++)
-    {
-        if(!Marked[j]&& IsRequestLessEqual(j))
-        {
+    // Go through Marked array and see which are unmarked, which are deadlocks
+    for(int j=0; j<numProcesses; j++){
+        if(!Marked[j]&& IsRequestLessEqual(j)){
             AddToAvailable(j);
             Marked[j] = true;
         }
-        
     }
     
       cout << "Deadlocked processes: ";
-    for(int j=0; j<numProcesses; j++)
-    {
-        if(!Marked[j])
-        {
+    for(int j=0; j<numProcesses; j++){
+        if(!Marked[j]){
             cout << j << " ";
-        }
-        
-    }
-    
-    
+        } 
+    } 
 }
 
 // ReadSystemConfig - read the system configuration from the
@@ -222,7 +185,8 @@ void ReadSystemConfig(const char *fileName) {
   }
 }
 
-} // namespace
+} 
+// namespace
 
 int main(int argc, char *argv[]) {
   // Read system configuration
@@ -231,11 +195,6 @@ int main(int argc, char *argv[]) {
     exit(1);
   }
   ReadSystemConfig(argv[1]);
-  //test IsRequestLessEqual
-  //IsRequestLessEqual(0);
-  //test AddToAvailable
-  //AddToAvailable(1);
-  
   PrintDeadlocks();
 
   return 0;
